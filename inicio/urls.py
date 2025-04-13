a295ed128da1ebbc3c93d891247d54aec65db70c
Daniel Romano <danielromano93@gmail.com>
2025-04-13 14:11:46 -0300

    Agregamos un estilo general a la web, manejando template base.

--- inicio/urls.py
+++ inicio/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from inicio.views import inicio, crear_participacion, lista_jugadores
 
 urlpatterns = [
-    path('', inicio, name='Inicio'),
+    path('', inicio, name='inicio'),
     path('crear-participacion/', crear_participacion, name='crear_participacion'),
     path('jugadores/', lista_jugadores, name='lista_jugadores'),
 ]
