745d775a6a0a69cbca2e5375353ec946a99f98b1
Daniel Romano <danielromano93@gmail.com>
2025-04-16 17:24:53 -0300

    Correción del model en Admin.

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Jugador, Participacion,Kill,TeamKill
+from .models import Jugador, Participacion,Kill,Teamkill
 
 
 admin.site.register(Jugador)
 admin.site.register(Participacion)
 admin.site.register(Kill)
-admin.site.register(TeamKill)
+admin.site.register(Teamkill)
