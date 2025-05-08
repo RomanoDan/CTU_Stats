bfba56e4ece2a9ecfc544b5c5a8968624a24c964
Daniel Romano <danielromano93@gmail.com>
2025-05-08 00:12:32 -0300

    Registro del modelo Partida en el panel de administración.

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Jugador, Participacion,Kill,Teamkill
+from .models import Jugador, Participacion,Kill,Teamkill,Partida
 
 
 admin.site.register(Jugador)
 admin.site.register(Participacion)
 admin.site.register(Kill)
 admin.site.register(Teamkill)
+admin.site.register(Partida)
